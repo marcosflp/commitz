f637a62cd1b15d961a022bb2a605f39e6fe96cda
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2020-01-02 18:34:33 -0300

    Add a new Login page
    - GitHub login only
    - Install and configure a new lib 'social-app-django'
    - Update readme

--- backend/commitz/urls.py
+++ backend/commitz/urls.py
@@ -1,13 +1,19 @@
-from django.conf.urls import include, url  # noqa
-from django.contrib import admin
-from django.views.generic import TemplateView
-
 import django_js_reverse.views
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^jsreverse/$', django_js_reverse.views.urls_js, name='js_reverse'),
+    url(r'^jsreverse/$', login_required(django_js_reverse.views.urls_js), name='js_reverse'),
 
-    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
+    url('^oauth/', include('social_django.urls', namespace='social')),
+    url('^login/$', LoginView.as_view(), name='login'),
+    url('^logout/$', LogoutView.as_view(), name='logout'),
+
+    url(r'^$', login_required(TemplateView.as_view(template_name='core/home.html')), name='home'),
 ]
