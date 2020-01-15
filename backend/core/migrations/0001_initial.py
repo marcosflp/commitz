c657b86d751d2257dd3275c539904b1263067563
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2020-01-15 14:54:41 -0300

    Fix migrations

--- backend/core/migrations/0001_initial.py
+++ backend/core/migrations/0001_initial.py
@@ -47,7 +47,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='CommitList',
+            name='Commit',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('sha', models.UUIDField()),
